c86acbc44e8a42eab1f26bd96a2520ea9b8295ca
Gabriel Martins <gabriel_alves_m@outlook.com.br>
2023-04-13 08:45:26 -0300

    Criação de um novo programa, e modificação do sorteio.

--- aula-06/sorteio.py
+++ aula-06/sorteio.py
@@ -3,8 +3,8 @@ from random import sample
 def sorteio_sem_repeticoes(n, i, f):
     return sorted(sample(range(i, f+1), n))
 
-# num = int(input("Deseja sortear quantos números? "))
-# ini = int(input("Digite o limite inicial do range: "))
-# fin = int(input("Digite o limite final do range: "))
+num = int(input("Deseja sortear quantos números? "))
+ini = int(input("Digite o limite inicial do range: "))
+fin = int(input("Digite o limite final do range: "))
 
-print(sorteio_sem_repeticoes(n=15, i=0, f=100))
+print(sorteio_sem_repeticoes(n=num, i=ini, f=fin))
